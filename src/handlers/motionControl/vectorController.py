cb5df741a6b48e903b424921cb714f753c2b0442
blj39 <blj39@cornell.edu>
2011-02-14 11:55:44 -0500

    Updated motion handlers: Updated skeletonController.py to correctly identify current region, and track region transitions. Included file (is_inside.py) to determine if the robot is inside a specified polygon. Indluded an alternative motion control handler (vectorController.py and vectorControllerHelper.py). Details:  - skeletonController.py: updated to include code for determining region transitions, and correctly outputing the 'arrived' variable 	(was previously output as True when the robot was assigned to stay in its current region, this was switched to False, as is 	used in the heat controller)  - is_inside.py: added this function to determine if the robot is in the specified region. 	The inputs are: 		* pose = [x, y] is a 2-D vector specifying the robot's location in the x and y coordinate system 		* vertices is a 2xN array specifying the region's vertices in a clockwise fashion, where each 2x1 pair is the [x, y] 			coordinates of the vertex 	The output is: 		* inside = 'True' if the robot is inside the specified region 			 = 'False' if the robot is outside the specified region  - vectorControllerHelper.py: added an alternate motion controller, similiar in nature and function to the heat-controller.  The vector 	controller smoothly combines several pose-varying vector fields according to the algorithm developed by Stephen R. Lindemann. 	The inputs are: 		* pose = [x, y] is a 2-D vector specifying the x-y position of the robot in the global frame 		* vert = vertices of the region, specified in clockwise order (2 x No. of vertices) 		* exit = index of exit face 		* last = True, if the current region is the last region 		       = False, if the current region is NOT the last region 	The output is: 		* V = [Vx, Vy] is the x-y velocity vector for the robot to exit its current region through the exit-face  - vectorController.py: added the handler function to find the desired velocity for the robot at its current location, and pass the 	information to the drive handler

--- src/handlers/motionControl/vectorController.py
+++ src/handlers/motionControl/vectorController.py
@@ -5,7 +5,9 @@ skeletonController.py - Skeleton Motion Controller
 ==================================================
 """
 
+import vectorControllerHelper
 from numpy import *
+from is_inside import *
 
 class motionControlHandler:
     def __init__(self, proj, shared_data):
@@ -36,17 +38,34 @@ class motionControlHandler:
         # that will get us on our way to the next region
 
         # NOTE: Information about region geometry can be found in self.rfi.regions:
-        pointArray = [x for x in self.rfi.regions[current].getPoints()]
+        pointArray = [x for x in self.rfi.regions[current_reg].getPoints()]
         pointArray = map(self.coordmap_map2lab, pointArray)
 	vertices = mat(pointArray).T 
+
+	if last:
+            transFace = None
+        else:
+            # Find a face to go through
+            # TODO: Account for non-determinacy?
+            pt1, pt2 = self.rfi.transitions[current_reg][next_reg][0]
+            
+            # Find the index of this face
+            # TODO: Why don't we just store this as the index?
+            for i, face in enumerate([x for x in self.rfi.regions[current_reg].getFaces()]):
+                # Account for both face orientations...
+                if (pt1 == face[0] and pt2 == face[1]) or (pt1 == face[1] and pt2 == face[0]):
+                    transFace = i
+                    break
+
+	V = vectorControllerHelper.getController([pose[0], pose[1]], vertices, transFace)
 
         [vx, vy, w] = [0, 0, 0]
 
         # Pass this desired velocity on to the drive handler
-        self.drive_handler.setVelocity(vx, vy, w)
+        self.drive_handler.setVelocity(V[0], V[1], pose[2])
         
         # TODO: Figure out whether we've reached the destination region
-
+	
 	if is_inside([pose[0], pose[1]], vertices):
 	        arrived = False
 	else:
